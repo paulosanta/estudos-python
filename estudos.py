a85fb4b69a770232bda3c0d8b279528771b76ea6
paulosanta <paulosneto7@gmail.com>
2024-10-04 00:19:30 -0300

    média aritmética
    Criei um sistema que calcula as 4 notas do aluno e retorna a média com uma condicional if else.

---
+++ estudos.py
@@ -0,0 +1,24 @@
+#primeira unidade: 7
+#segunda unidade: 4
+#terceira unidade: 6
+#quarta unidade: 8
+
+
+
+aluno = 'jonas'
+nota1 = 7
+nota2 = 8
+nota3 = 6
+nota4 = 8
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+
+limite = 7
+
+if (media) >= limite:
+    print('passou, sua média foi', media)
+    print(aluno, 'você é 10!')
+else:
+    print('reprovou')
+    print(aluno, 'você precisa estudar mais, sua média foi', media)
+
